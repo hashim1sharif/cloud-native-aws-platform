d430460ed3d19d439c910587cde40da27a5d8ad3
hashim1sharif <haashimsh@hotmail.com>
2026-07-27 14:55:10 +0200

    add isolated database routing

--- infra/terraform/environments/dev/route_tables.tf
+++ infra/terraform/environments/dev/route_tables.tf
@@ -25,8 +25,6 @@ resource "aws_route_table_association" "public_subnet_2_route_table_association"
 }
 
 
-
-
 # Creating a route table for the private application subnets
 resource "aws_route_table" "private_app_route_table" {
   vpc_id = aws_vpc.vpc.id
@@ -51,4 +49,25 @@ resource "aws_route_table_association" "private_app_subnet_1_route_table_associa
 resource "aws_route_table_association" "private_app_subnet_2_route_table_association" {
   subnet_id      = aws_subnet.private_app_subnet_2.id
   route_table_id = aws_route_table.private_app_route_table.id
+}
+
+# Creating an isolated route table for the private database subnets
+resource "aws_route_table" "private_db_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${local.project_environment_name}-private-db-route-table"
+  }
+}
+
+# Associating private database subnet 1 with the database route table
+resource "aws_route_table_association" "private_db_subnet_1_route_table_association" {
+  subnet_id      = aws_subnet.private_db_subnet_1.id
+  route_table_id = aws_route_table.private_db_route_table.id
+}
+
+# Associating private database subnet 2 with the database route table
+resource "aws_route_table_association" "private_db_subnet_2_route_table_association" {
+  subnet_id      = aws_subnet.private_db_subnet_2.id
+  route_table_id = aws_route_table.private_db_route_table.id
 }
