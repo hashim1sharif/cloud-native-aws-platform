fe4f15d7dcabf3379c318c5b311214705d7beafa
hashim1sharif <haashimsh@hotmail.com>
2026-07-28 21:43:42 +0200

    allow ECS to read RDS credentials

--- infra/terraform/environments/dev/ecs_iam.tf
+++ infra/terraform/environments/dev/ecs_iam.tf
@@ -24,3 +24,27 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# Allows ECS to inject the RDS-managed credentials into the backend container
+resource "aws_iam_role_policy" "ecs_task_execution_secrets_policy" {
+  name = "${local.project_environment_name}-ecs-secrets-access"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+
+        Resource = [
+          aws_db_instance.postgres_db_instance.master_user_secret[0].secret_arn
+        ]
+      }
+    ]
+  })
+}
