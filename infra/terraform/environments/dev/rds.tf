5ccf2c461b3255ab371dc32718d815716a4d9328
hashim1sharif <haashimsh@hotmail.com>
2026-07-28 15:54:16 +0200

    add PostgreSQL RDS instance

---
+++ infra/terraform/environments/dev/rds.tf
@@ -0,0 +1,22 @@
+# Creates the private PostgreSQL database for the application
+resource "aws_db_instance" "postgres_db_instance" {
+  identifier = "${local.project_environment_name}-postgres"
+
+  engine         = "postgres"
+  engine_version = "16"
+  instance_class = "db.t4g.micro"
+
+  allocated_storage = 20
+  storage_encrypted = true
+
+  db_name  = "devops_tasks"
+  username = "postgres"
+
+  manage_master_user_password = true
+
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.database_security_group.id]
+  publicly_accessible    = false
+
+  skip_final_snapshot = true
+}
