a5c0e92e4f3402af8d2ca14c8bed3132003b4fab
hashim1sharif <haashimsh@hotmail.com>
2026-08-01 14:03:56 +0200

    Consolidate development module calls into main

--- infra/terraform/environments/dev/acm.tf
+++
@@ -1,7 +0,0 @@
-module "acm" {
-  source = "../../modules/acm"
-
-  domain_name    = local.application_domain_name
-  hosted_zone_id = data.aws_route53_zone.primary_hosted_zone.zone_id
-  tags           = local.common_tags
-}
--- infra/terraform/environments/dev/security_groups.tf
+++
@@ -1,6 +0,0 @@
-module "security" {
-  source = "../../modules/security"
-
-  name_prefix = local.project_environment_name
-  vpc_id      = module.network.vpc_id
-}
